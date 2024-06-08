b917ca4b3d427dda242ea78ebca4d89b98b6595c
Victoria <vika96karnauhova@gmail.com>
2024-06-09 00:47:01 +0300

    task-6 done

--- js/task-5.js
+++ js/task-5.js
@@ -7,9 +7,18 @@
 // Фон на <body> задається тільки після кліку на button.change-color
 // При кожному кліку на елемент button.change-color фон <body> зафарбовується новим рандомним кольором
 // На <body> і span.color значення одного й того самого кольору
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorBody = document.querySelector('body');
+const colorSpan = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
+button.addEventListener('click', () => {
+  const newRandomColor = getRandomHexColor();
+  colorBody.style.backgroundColor = newRandomColor;
+  colorSpan.textContent = newRandomColor;
+});
